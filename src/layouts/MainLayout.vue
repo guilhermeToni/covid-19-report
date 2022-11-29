82803e7e58c2303b60224d01ca31de2fd31b1a8e
Guilherme Toni <guilhermedelly8@gmail.com>
2022-11-29 19:10:46 -0300

    :tada: Add project base

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,35 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen" />
+
+        <q-toolbar-title>
+          Covid-19 Report
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: 'MainLayout',
+  components: {},
+  data() {
+    return {
+      leftDrawerOpen: false,
+    };
+  },
+};
+</script>
