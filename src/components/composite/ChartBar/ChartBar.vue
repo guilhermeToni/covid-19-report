732816bfb2f8a8e149169b8ecd84405ad5331269
Guilherme Toni <guilhermedelly8@gmail.com>
2022-11-30 09:57:22 -0300

    :sparkles: Add chartBar composite component

---
+++ src/components/composite/ChartBar/ChartBar.vue
@@ -0,0 +1,80 @@
+<template>
+  <Bar
+    :chart-data="chartData"
+    :chart-id="chartId"
+    :chart-options="chartOptions"
+    :css-classes="cssClasses"
+    :dataset-id-key="datasetIdKey"
+    :height="height"
+    :plugins="plugins"
+    :styles="styles"
+    :width="width" />
+</template>
+
+<script>
+import { Bar } from 'vue-chartjs/legacy';
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
+
+export default {
+  name: 'BarChart',
+  components: {
+    Bar,
+  },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart',
+    },
+    datasetIdKey: {
+      type: String,
+      default: 'label',
+    },
+    width: {
+      type: Number,
+      default: 400,
+    },
+    height: {
+      type: Number,
+      default: 400,
+    },
+    cssClasses: {
+      default: '',
+      type: String,
+    },
+    styles: {
+      type: Object,
+      default: () => {},
+    },
+    plugins: {
+      type: Array,
+      default: () => [],
+    },
+    chartData: {
+      type: Object,
+      default: () => ({
+        labels: [],
+        datasets: [],
+      }),
+    },
+  },
+  data() {
+    return {
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: true,
+      },
+    };
+  },
+};
+</script>
